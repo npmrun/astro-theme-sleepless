cbd3e171e412614d313c17ca67cc4f7c5daf0f0f
npmrun <1549469775@qq.com>
2023-08-26 02:00:38 +0800

    add

---
+++ src/assets/script/main.ts
@@ -0,0 +1,21 @@
+if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
+
+function toggleTheme() {
+    if (!document.documentElement.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark')
+        document.documentElement.classList.add('dark')
+    } else {
+        localStorage.removeItem('theme')
+        document.documentElement.classList.remove('dark')
+    }
+}
+
+window.toggleTheme = toggleTheme
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,9 @@
+export const SiteInfo = {
+    title: '失眠男士',
+    description: '花间一壶酒，独酌无相亲。举杯邀明月，对影成三人。',
+}
 
-export const BlogCollectionName = 'posts';
+export const BlogCollectionName = 'posts'
 export const getBlogSlugPrefix = (url: string) => {
-  return '/posts/' + url;
-};
+    return '/posts/' + url
+}
