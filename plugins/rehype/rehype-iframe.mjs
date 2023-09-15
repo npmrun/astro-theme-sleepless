46d72971a2faed5932a91d26eeac1ad9e16a2bbd
npmrun <1549469775@qq.com>
2023-09-15 11:37:27 +0800

    add iframe style

--- plugins/rehype/rehype-iframe.mjs
+++ plugins/rehype/rehype-iframe.mjs
@@ -1,5 +1,33 @@
 import { visitParents, CONTINUE, EXIT, SKIP } from 'unist-util-visit-parents'
 
+function buildNode (url) {
+    return {
+        type: "raw",
+        value: `<figure class="iframe-wrapper">
+        <div class="iframe-title">
+      <!--   <div class="text">
+          网页
+          </div> -->
+          <div class="buttons">
+            <div class="button" onclick="window.open('${url}')">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="currentColor" d="M6 4a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-2.5a.5.5 0 0 1 1 0V14a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h2.5a.5.5 0 0 1 0 1H6Zm5-.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V4.707l-4.146 4.147a.5.5 0 0 1-.708-.708L15.293 4H11.5a.5.5 0 0 1-.5-.5Z"/></svg>
+          </div>
+      <!--     <div class="button" onclick="document.getElementById('iframeId').src='${url}'">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="currentColor" d="M14.66 15.66A8 8 0 1 1 17 10h-2a6 6 0 1 0-1.76 4.24l1.42 1.42zM12 10h8l-4 4l-4-4z"/></svg>
+          </div> -->
+          </div>
+        </div>
+        <div class="iframe-inner">
+          <iframe width="100%" src="${url}">
+            <div>
+            不支持iframe
+            </div>
+          </iframe>
+        </div>
+      </figure>`
+    }
+}
+
 export default function () {
     return async (tree) => {
         visitParents(tree, 'raw', (node, ancestors) => {
@@ -17,15 +45,7 @@ export default function () {
                             break
                         }
                     }
-                    parent.children[index] = {
-                        type: "element",
-                        tagName: "iframe",
-                        properties: {
-                            width: `100%`,
-                            height: `300px`,
-                            src: url
-                        },
-                    }
+                    parent.children[index] = buildNode(url)
                     return SKIP
                 }
             }
