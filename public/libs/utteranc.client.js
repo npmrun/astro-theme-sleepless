20da304de0dc3caa4382cede86ec6382c7a7cfd1
xieyaxin <1549469775@qq.com>
2024-05-13 10:24:18 +0800

    优化评论加载中

--- public/libs/utteranc.client.js
+++ public/libs/utteranc.client.js
@@ -11,10 +11,34 @@ script.onerror = function () {
     if (sc) {
         sc.innerText = "评论加载失败，刷新重试";
     }
+    clearInterval(checkId)
 };
 var sc = document.getElementById("utteranc-sc");
 // var parentNode = sc.parentNode;
 sc.appendChild(script);
+
+function checkSuccess() {
+    clearInterval(checkId)
+    var el = document.querySelector(".comment-el")
+    if(el) {
+        el.remove()
+    }
+}
+var commentLoadingTime = 0; 
+let checkId = setInterval(() => {
+    var el = sc.querySelector(".utterances")
+    if(el && el.getAttribute("style")) {
+        checkSuccess()
+    } else {
+        commentLoadingTime += 1
+        if (commentLoadingTime > 20) {
+            clearInterval(checkId);
+            if (sc) {
+                sc.innerText = "评论加载失败，刷新重试";
+            }
+        }
+    }
+}, 500);
 
 if (document.currentScript && document.currentScript.parentNode) {
     document.currentScript.parentNode.removeChild(document.currentScript);
